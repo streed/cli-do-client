945704f71ea6a9479f70dfa6ab04ef2d3a96d1d7
Reed <streed@users.noreply.github.com>
2024-08-02 09:49:09 +0000

    Fix some of the todo editing code. Add a --all to todo list and hit the todos endpoint

--- cli-do.go
+++ cli-do.go
@@ -156,7 +156,13 @@ func main() {
 					{
 						Name:    "list",
 						Aliases: []string{"ls"},
-						Action:  HandleTodosList,
+						Flags: []cli.Flag{
+							&cli.BoolFlag{
+								Name:    "all",
+								Aliases: []string{"a"},
+							},
+						},
+						Action: HandleTodosList,
 					},
 					{
 						Name:      "get",
@@ -337,10 +343,12 @@ func HandleTodosList(ctx *cli.Context) error {
 		return nil
 	}
 
+	var all = ctx.Bool("all")
+
 	resp, err := resty.New().R().
 		SetHeader("Authorization", fmt.Sprintf("Bearer %s", auth.AccessToken)).
 		SetHeader("Content-Type", "application/json").
-		Get(fmt.Sprintf("%s/projects/%s", config.Endpoint, directorySettings.ProjectId))
+		Get(fmt.Sprintf("%s/projects/%s/todos?all=%t", config.Endpoint, directorySettings.ProjectId, all))
 
 	if err != nil {
 		fmt.Println("Error:", err)
@@ -719,6 +727,8 @@ func ParseTempTodoFile(todo Todo, path string) (Todo, error) {
 		return todo, err
 	}
 
+	defer file.Close()
+
 	fileScanner := bufio.NewScanner(file)
 	fileScanner.Split(bufio.ScanLines)
 	var fileLines []string
@@ -727,22 +737,18 @@ func ParseTempTodoFile(todo Todo, path string) (Todo, error) {
 		fileLines = append(fileLines, fileScanner.Text())
 	}
 
-	file.Close()
+	var updatedTodo = ParseHeaders(todo, fileLines[:4])
 
-	var updatedTodo, headerEnd = ParseHeaders(todo, fileLines)
-
-	var newBody = strings.Join(fileLines[headerEnd:], "\n")
+	var newBody = strings.Join(fileLines[4:], "\n")
 	updatedTodo.Body = newBody
 
 	return updatedTodo, nil
 }
 
-func ParseHeaders(todo Todo, fileLines []string) (Todo, int) {
+func ParseHeaders(todo Todo, fileLines []string) Todo {
 	regex := regexp.MustCompile(`#\s+(Ticket|Subject|Completed|DueDate)\s*:\s+(.*)`)
-	var lineNum = 0
 
 	for _, line := range fileLines {
-		lineNum = lineNum + 1
 		if line == "" {
 			break
 		}
@@ -762,27 +768,38 @@ func ParseHeaders(todo Todo, fileLines []string) (Todo, int) {
 		}
 
 		if matches[1] == "DueDate" {
-			t, _ := time.Parse("2006-01-02", matches[2])
-			todo.DueDate = &t
+			if matches[2] != "none" {
+				t, _ := time.Parse("2006-01-02", matches[2])
+				todo.DueDate = &t
+			}
 		}
 	}
 
-	return todo, lineNum
+	return todo
 }
 
 func WriteToTempFile(todo Todo) (string, error) {
 	var file, err = os.CreateTemp("./", ".todo-*")
+	defer file.Close()
+
 	if err != nil {
 		return "", err
 	}
 
-	var header = fmt.Sprintf("# Ticket: %d\n# Subject: %s\n# Completed: %t\n", todo.Ticket, todo.Subject, todo.Completed)
+	var header = fmt.Sprintf("# Ticket: %d\n# Subject: %s\n# Completed: %t\n",
+		todo.Ticket,
+		todo.Subject,
+		todo.Completed)
 
 	if todo.DueDate != nil {
-		header = fmt.Sprintf("%s# DueDate: %s", header, todo.DueDate.Format("2006-01-02"))
+		header = fmt.Sprintf("%s# DueDate: %s\n", header, todo.DueDate.Format("2006-01-02"))
+	} else {
+		header = fmt.Sprintf("%s# DueDate: none\n", header)
 	}
 
-	header = fmt.Sprintf("%s\n\n", header)
+	if todo.Body == "" || todo.Body == "\n" {
+		header = fmt.Sprintf("%s\n", header)
+	}
 
 	_, err = file.WriteString(header + todo.Body)
 
